369c3539a7ff407de7736c9f82901f1ed1c37884
adit777 <aditya.aulia09@gmail.com>
2020-09-09 17:49:35 +0700

    Updates

---
+++ precache-manifest.8907d9801cfc77dd5041085aed1c972f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4281fa3ced98205be00a891ffabe0b3",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "0da555a76fdd02ad272f",
+    "url": "/react-todolist/static/css/main.07179b94.chunk.css"
+  },
+  {
+    "revision": "cae303a678086002c6b6",
+    "url": "/react-todolist/static/js/2.4109f84e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todolist/static/js/2.4109f84e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0da555a76fdd02ad272f",
+    "url": "/react-todolist/static/js/main.3f14e207.chunk.js"
+  },
+  {
+    "revision": "dea162c310a378ff9d0d",
+    "url": "/react-todolist/static/js/runtime-main.82f4965a.js"
+  }
+]);
